b94fcebda0d61edb8bb74d7c4ed67bc9bfbcb5b4
CSComarch <rajkonkret660@gmail.com>
2023-11-29 09:55:24 +0100

    2 - add kl5

---
+++ 3.2 kl5.py
@@ -0,0 +1,30 @@
+from typing import Optional
+
+
+class Sample:
+    def __init__(self,
+                 a: int,
+                 b: int,
+                 c: int,
+                 d: int,
+                 e: Optional[str] = None) -> None:
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+        self.e = e
+
+    # __repr__
+    def __repr__(self):
+        e_repr = "tekst" if self.e is None else self.e
+        return (f"a= {self.a},"
+                f"b= {self.b},"
+                f"c= {self.c},"
+                f"d= {self.d},"
+                f"e={e_repr}")
+
+
+ob = Sample(1, 2, 3, 4, 5)
+print(ob)
+ob2 = Sample(1, 2, 3, 4)
+print(ob2)  # a= 1,b= 2,c= 3,d= 4,e=tekst
