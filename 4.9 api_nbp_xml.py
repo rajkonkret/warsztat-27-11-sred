ec869605cf2a427ba6c909296dfc9b0a462f3a42
CSComarch <rajkonkret660@gmail.com>
2023-11-30 15:32:17 +0100

    4 - update apiget nbm xml

--- 4.9 api_nbp_xml.py
+++ 4.9 api_nbp_xml.py
@@ -1,7 +1,24 @@
+from typing import List
+
 import requests
 import xml.etree.ElementTree as ET
+from pydantic import BaseModel
 
 url = 'http://api.nbp.pl/api/exchangerates/tables/A/?format=xml'
+
+
+class CurrencyRate(BaseModel):
+    currency: str
+    code: str
+    mid: float
+
+
+class ExchangeRatesTable(BaseModel):
+    table: str
+    date: str
+    number: str
+    rates: List[CurrencyRate]
+
 
 response = requests.get(url)
 xml_data = response.content
@@ -22,8 +39,15 @@ print(f"Numer tabeli: {no}")  # Numer tabeli: 232/A/NBP/2023
 rates = root.findall('.//Rate')
 print(rates)
 
+currency_rates = []
 for rate in rates:
     currency = rate.find('Currency').text
     code = rate.find('Code').text
     mid = float(rate.find('Mid').text)
-# 15:10
+    # 15:10
+    print(f"Currency: {currency}, Code: {code}, Mid: {mid}")
+    currency_rates.append(CurrencyRate(currency=currency, code=code, mid=mid))
+
+exchange_rate_table = ExchangeRatesTable(table=table_name, date=data, number=no, rates=currency_rates)
+print(exchange_rate_table)
+print(exchange_rate_table.rates[0])  # currency='bat (Tajlandia)' code='THB' mid=0.1131
