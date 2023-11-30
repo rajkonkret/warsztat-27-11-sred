77f27f547c2e046cc8beb80e3bccdb0422c0f341
CSComarch <rajkonkret660@gmail.com>
2023-11-30 10:18:30 +0100

    3 -add baza3

---
+++ 4.1 baza3 - sqlalchemy.py
@@ -0,0 +1,6 @@
+# sqlalchemy - sytem (biblioteka) orm do wspołpracy z baza danych za pomocą progrmowania obiektowego
+# pip nstall nazwa_biblioteki
+from sqlalchemy import (
+    Column, Integer, String, ForeignKey, create_engine
+)
+# 10:45
